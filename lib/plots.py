c1feba7d72d3477543bf06be3d2f0d8ec034738c
Troy Stribling <troy.stribling@gmail.com>
2022-03-20 14:31:01 -0700

    finished first pass on plot refactor

--- lib/plots.py
+++ lib/plots.py
@@ -15,47 +15,109 @@ class PlotType(Enum):
     XLOG = 3
     YLOG = 4
 
-def time_series(samples, time, title):
-    figure, axis = pyplot.subplots(figsize=(15, 12))
-    axis.set_xlabel(r"$t$")
-    axis.set_ylabel(r"$X_t$")
-    axis.set_title(title)
-    axis.plot(time, samples, lw=1)
+class PlotDataType(Enum):
+    TIME_SERIES = 1
+    PSPEC = 2
 
-def pspec(ps, time, title):
-    figure, axis = pyplot.subplots(figsize=(15, 12))
-    axis.set_xlabel(r"$\omega$")
-    axis.set_ylabel(r"$\rho_\omega$")
-    axis.set_title(title)
-    logStyle(axis, ps)
-    axis.loglog(time, ps, lw=1)
+def curve(x, y, title, data_type=PlotDataType.TIME_SERIES):
+    if data_type == PlotDataType.TIME_SERIES:
+        plot_type = PlotType.LINEAR
+        xlabel = r"$t$"
+        ylabel = r"$X_t$"
+    elif data_type == PlotDataType.PSPEC:
+        plot_type = PlotType.LOG
+        xlabel = r"$\omega$"
+        ylabel = r"$\rho_\omega$"
+    else:
+        plot_type = PlotType.LINEAR
+        xlabel = "y"
+        ylabel = "x"
 
-def time_series_comparison(samples, time, labels, lengend_location, title):
-    nplot = len(samples)
     figure, axis = pyplot.subplots(figsize=(15, 12))
-    axis.set_xlabel(r"$t$")
-    axis.set_ylabel(r"$X_t$")
     axis.set_title(title)
+    axis.set_xlabel(xlabel)
+    axis.set_ylabel(ylabel)
+
+    if plot_type == PlotType.LOG:
+        _logStyle(axis, y)
+        axis.loglog(x, y, lw=1)
+    elif plot_type == PlotType.XLOG:
+        _logXStyle(axis, y)
+        axis.semilogx(x, y, lw=1)
+    elif plot_type == PlotType.YLOG:
+        _logYStyle(axis, y)
+        axis.semilog(x, y, lw=1)
+    else:
+        axis.plot(x, y, lw=1)
+
+def comparison(x, y, labels, lengend_location, title, data_type=PlotDataType.TIME_SERIES):
+    if data_type == PlotDataType.TIME_SERIES:
+        plot_type= PlotType.LINEAR
+        xlabel = r"$t$"
+        ylabel = r"$X_t$"
+    elif data_type == PlotDataType.PSPEC:
+        plot_type = PlotType.LOG
+        xlabel = r"$\omega$"
+        ylabel = r"$\rho_\omega$"
+    else:
+        plot_type = PlotType.LINEAR
+        xlabel = "y"
+        ylabel = "x"
+
+    nplot = len(y)
+    figure, axis = pyplot.subplots(figsize=(15, 12))
+    axis.set_xlabel(xlabel)
+    axis.set_ylabel(ylabel)
+    axis.set_title(title)
+
     for i in range(nplot):
-        axis.plot(time, samples[i], lw=1, label=labels[i])
+        if plot_type == PlotType.LOG:
+            axis.loglog(x, y[i], lw=1, label=labels[i])
+        elif plot_type == PlotType.XLOG:
+            _logXStyle(axis, ps)
+            axis.semilogx(x, y[i], lw=1, label=labels[i])
+        elif plot_type == PlotType.YLOG:
+            _logYStyle(axis, ps)
+            axis.semilogy(x, y[i], lw=1, label=labels[i])
+        else:
+            axis.plot(x, y[i], lw=1, label=labels[i])
+
     axis.legend(ncol=2, bbox_to_anchor=lengend_location)
 
-def time_series_stack(series, ylim, labels, title, time=None):
-    nplot = len(series)
-    if time is None:
-        nsample = len(series[0])
-        time = numpy.tile(numpy.linspace(0, nsample-1, nsample), (nplot,1))
+def stack(y, ylim, labels, title, x=None, data_type=PlotDataType.TIME_SERIES):
+    if data_type == PlotDataType.TIME_SERIES:
+        plot_type = PlotType.LINEAR
+        xlabel = r"$t$"
+        ylabel = r"$X_t$"
+    else:
+        plot_type = PlotType.LINEAR
+        xlabel = "y"
+        ylabel = "x"
+
+    nplot = len(y)
+    if x is None:
+        nx = len(y[0])
+        x = numpy.tile(numpy.linspace(0, nx-1, nx), (nplot,1))
+
     figure, axis = pyplot.subplots(nplot, sharex=True, figsize=(15, 12))
     axis[0].set_title(title)
-    axis[nplot-1].set_xlabel(r"$t$")
+    axis[nplot-1].set_xlabel(xlabel)
+
     for i in range(nplot):
-        nsample = len(series[i])
-        axis[i].set_ylabel(r"$X_t$")
+        nsample = len(y[i])
+        axis[i].set_ylabel(ylabel)
         axis[i].set_ylim(ylim)
-        axis[i].set_xlim([0.0, time[i][-1]])
-        text = axis[i].text(time[i][int(0.9*nsample)], 0.65*ylim[-1], labels[i], fontsize=18)
+        axis[i].set_xlim([0.0, x[i][-1]])
+        text = axis[i].text(x[i][int(0.9*nsample)], 0.65*ylim[-1], labels[i], fontsize=18)
         text.set_bbox(dict(facecolor='white', alpha=0.75, edgecolor='white'))
-        axis[i].plot(time[i], series[i], lw=1.0)
+        if plot_type == PlotType.LOG:
+            axis[i].loglog(x[i], y[i], lw=1.0)
+        elif plot_type == PlotType.XLOG:
+            axis[i].semilogx(x[i], y[i], lw=1.0)
+        elif plot_type == PlotType.YLOG:
+            axis[i].semilogy(x[i], y[i], lw=1.0)
+        else:
+            axis[i].plot(x[i], y[i], lw=1.0)
 
 def cumulative(accum, target, title, label):
     range = max(accum) - min(accum)
@@ -138,7 +200,7 @@ def regression(y, x, results, title, type=RegressionPlotType.LINEAR):
     axis.text(x_text, y_text, results_text, bbox=bbox, fontsize=16.0, zorder=7, transform=axis.transAxes)
 
     if plot_type == PlotType.LOG:
-        logStyle(axis, x)
+        _logStyle(axis, x)
         axis.loglog(x, y, marker='o', markersize=5.0, linestyle="None", markeredgewidth=1.0, alpha=0.75, zorder=5, label="Data")
         axis.loglog(x, y_fit, zorder=10, label=label)
     else:
@@ -150,10 +212,24 @@ def regression(y, x, results, title, type=RegressionPlotType.LINEAR):
 def logspace(npts, max, min=10.0):
     return numpy.logspace(numpy.log10(min), numpy.log10(max/min), npts)
 
-def logStyle(axis, x):
+### -- Private ---
+def _logStyle(axis, x):
     if numpy.log10(max(x)/min(x)) < 3:
-        axis.tick_params(axis='both', which='minor', length=8, color="#c0c0c0", direction="in")
-        axis.tick_params(axis='both', which='major', length=15, color="#c0c0c0", direction="in", pad=10)
-        axis.spines['bottom'].set_color("#c0c0c0")
-        axis.spines['left'].set_color("#c0c0c0")
+        axis.tick_params(axis='both', which='minor', length=8, color="#b0b0b0", direction="in")
+        axis.tick_params(axis='both', which='major', length=15, color="#b0b0b0", direction="in", pad=10)
+        axis.spines['bottom'].set_color("#b0b0b0")
+        axis.spines['left'].set_color("#b0b0b0")
         axis.set_xlim([min(x), max(x)])
+
+def _logXStyle(axis, x):
+    if numpy.log10(max(x)/min(x)) < 3:
+        axis.tick_params(axis='x', which='minor', length=8, color="#b0b0b0", direction="in")
+        axis.tick_params(axis='x', which='major', length=15, color="#b0b0b0", direction="in", pad=10)
+        axis.spines['bottom'].set_color("#b0b0b0")
+        axis.set_xlim([min(x), max(x)])
+
+def _logYStyle(axis, x):
+    if numpy.log10(max(x)/min(x)) < 3:
+        axis.tick_params(axis='y', which='minor', length=8, color="#b0b0b0", direction="in")
+        axis.tick_params(axis='y', which='major', length=15, color="#b0b0b0", direction="in", pad=10)
+        axis.spines['left'].set_color("#b0b0b0")
