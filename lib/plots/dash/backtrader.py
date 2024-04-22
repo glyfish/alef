d647c0aebb27c8a785a357b434164bc4acf9029c
Troy Stribling <troy.stribling@gmail.com>
2024-04-21 18:47:19 -0700

    added a couple more dashboards

--- lib/plots/dash/backtrader.py
+++ lib/plots/dash/backtrader.py
@@ -141,9 +141,80 @@ def position(data: DataFrame, **kwargs):
     value = price * position
 
     date = data.date.to_numpy()
-    ticker = data.ticker[0]
+    ticker = data.ticker.iloc[0]
+    run_id = data.run_id.iloc[0]
+    ensemble_id = data.ensemble_id.iloc[0]
     
-    title = f"{ticker} Position Size and Value"
+    title = f"{ticker} Position Size and Value\nRun ID: {run_id}, Ensemble ID: {ensemble_id}"
 
     comp.bar(axis[0], position, date, xlabel=None, title=title, ylabel="Size", alpha=1.0)
     comp.bar(axis[1], value, date, xlabel="Date", ylabel="Dollars", alpha=1.0)
+
+
+def orders(order_data: DataFrame, asset_price_data: DataFrame, **kwargs):
+    """
+    Plot order size and value time series.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Data to plot.
+    symbol_offset_factor : float
+        Symbol offset factor.
+    figsize : Tuple[int, int]
+        Figure size.
+    """
+
+    figsize = get_param_default_if_missing("figsize", (10,6), **kwargs)
+    _, axis = pyplot.subplots(figsize=figsize, sharex=True, sharey=False)
+
+    price = asset_price_data.close_price.to_numpy()
+    price_date = asset_price_data.date.to_numpy()
+    offset = (numpy.max(price) - numpy.min(price)) * 0.05
+
+    buy_orders = order_data.query('order_type == "Buy" and order_status == "Completed"')
+    sell_orders = order_data.query('order_type == "Sell" and order_status == "Completed"')
+
+    buy_price = buy_orders.price.to_numpy() - offset
+    buy_date = buy_orders.date.to_numpy()
+    sell_price = sell_orders.price.to_numpy() + offset
+    sell_date = sell_orders.date.to_numpy()
+
+    ticker = sell_orders.ticker.iloc[0]
+    run_id = sell_orders.run_id.iloc[0]
+    ensemble_id = sell_orders.ensemble_id.iloc[0]
+    
+    title = f"{ticker} Order Size and Value\nRun ID: {run_id}, Ensemble ID: {ensemble_id}"
+
+    comp.fcurve_scatter_comparison(axis, [buy_price, sell_price], price, [buy_date, sell_date], price_date, title=title, xlabel='Date', 
+                                    ylabel='Price', lw=1, labels=[ticker, 'Buy', 'Sell'], markers=['^', 'v'], marker_colors=['#007735', '#BB0000'],
+                                    marker_size=6.0) 
+
+
+def pnl(data: DataFrame, **kwargs):
+    """
+    Plot profit and loss time series.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Data to plot.
+    figsize : Tuple[int, int]
+        Figure size.
+    """
+
+    figsize = get_param_default_if_missing("figsize", (10,8), **kwargs)
+    _, axis = pyplot.subplots(2, figsize=figsize, sharex=True, sharey=False)
+
+    pnl = data.pnl.to_numpy()
+    pnl_date = data.date.to_numpy()
+    ticker = data.ticker.iloc[0]
+    run_id = data.run_id.iloc[0]
+    ensemble_id = data.ensemble_id.iloc[0]
+
+    cumulative_pnl = data.pnl.cumsum().to_numpy()
+
+    title = f"{ticker} Profit and Loss\nRun ID: {run_id}, Ensemble ID: {ensemble_id}"
+
+    comp.positive_negative_bar(axis[0], pnl, pnl_date, title=title, ylabel='PnL (Dollars)', alpha=1.0, xlabel=None)
+    comp.curve(axis[1], cumulative_pnl, pnl_date, ylabel='Value (Dollars)', xlabel='Date')
