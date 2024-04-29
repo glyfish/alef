6dfd75c99c220bf692b217d8313293c0f0d411c7
Troy Stribling <troy.stribling@gmail.com>
2024-04-28 17:25:08 -0700

    refactored backtrader plots, started dashboard plot

--- lib/plots/__init__.py
+++ lib/plots/__init__.py
@@ -5,6 +5,6 @@ from lib.plots.dash import  (curve, comparison, stack, twinx, twinx_comparison, 
                              colored_scatter_contour, colored_scatter, fscatter, twinx_bar, twinx_bar_line,
                              fcurve_scatter_comparison, positive_negative_bar)
 
-from lib.plots.dash.backtrader import (price_series, asset_price, zscore_indicator, cash_value)
+from lib.plots.dash.backtrader import (price_series, asset_price, zscore_indicator, cash_value, zscore_backtest)
 
 from lib.plots.comp.axis import (PlotType)
--- lib/plots/dash/backtrader.py
+++ lib/plots/dash/backtrader.py
@@ -1,10 +1,12 @@
 from matplotlib import pyplot
+import matplotlib.gridspec as gridspec
 import numpy
 from pandas import DataFrame
 
 from lib.utils import get_param_default_if_missing
 from lib.plots import comp
 from typing import Callable
+
 
 def price_series(data: DataFrame, **kwargs):
     """
@@ -20,7 +22,6 @@ def price_series(data: DataFrame, **kwargs):
 
     figsize = get_param_default_if_missing("figsize", (10,6), **kwargs)
     _, axis = pyplot.subplots(figsize=figsize)
-
 
     date = data.date.to_numpy()
     close = data.close_price.to_numpy()
@@ -48,16 +49,13 @@ def asset_price(data: DataFrame, **kwargs):
     figsize = get_param_default_if_missing("figsize", (10,6), **kwargs)
     _, axis = pyplot.subplots(figsize=figsize)
 
-    date = data.date.to_numpy()
-    close = data.close_price.to_numpy()
     ticker = data.ticker[0]
     run_id = data.run_id[0]
     ensemble_id = data.ensemble_id[0]
 
-
     title = f"{ticker} Asset Price Series\nRun ID: {run_id}, Ensemble ID: {ensemble_id}"
 
-    comp.curve(axis, close, date, title=title, xlabel="Date", ylabel="Price", label=ticker, lw=1)
+    __asset_price(axis, data, title=title)
 
 
 def zscore_indicator(data: DataFrame, mean_reversion_half_life: int, **kwargs):
@@ -77,19 +75,14 @@ def zscore_indicator(data: DataFrame, mean_reversion_half_life: int, **kwargs):
     figsize = get_param_default_if_missing("figsize", (10,6), **kwargs)
     _, axis = pyplot.subplots(figsize=figsize)
 
-    date = data.date.to_numpy()
-    zscore = data.zscore.to_numpy()
     ticker = data.ticker[0]
     mean_reversion_half_life = int(mean_reversion_half_life)
     run_id = data.run_id[0]
     ensemble_id = data.ensemble_id[0]
 
-    zero = numpy.full(len(zscore), 0.0)
-
     title = f"{ticker}, Z-Score Indicator Time Series, $t_{{1/2}}$={mean_reversion_half_life}\nRun ID: {run_id}, Ensemble ID: {ensemble_id}"
 
-    comp.comparison(axis, [zscore[mean_reversion_half_life:], zero], date[mean_reversion_half_life:], title=title, xlabel="Date", 
-                    ylabel="Z-Score", lw=1)
+    __zscore_indicator(axis, data, mean_reversion_half_life, title=title)  
 
 
 def cash_value(data: DataFrame, **kwargs):
@@ -107,17 +100,12 @@ def cash_value(data: DataFrame, **kwargs):
     figsize = get_param_default_if_missing("figsize", (10,6), **kwargs)
     _, axis = pyplot.subplots(figsize=figsize)
 
-    date = data.date.to_numpy()
-    cash = data.cash.to_numpy()
-    value = data.value.to_numpy()
-    spend = value - cash
     run_id = data.run_id[0]
     ensemble_id = data.ensemble_id[0]
 
     title = f"Account Balance\nRun ID: {run_id}, Ensemble ID: {ensemble_id}"
 
-    comp.comparison(axis, [cash, value, spend], date, title=title, xlabel="Date", ylabel="Dollars", lw=1, 
-                    labels=["Cash", "Value", "Spend"])
+    __cash_value(axis, data, title=title)
 
 
 def position(data: DataFrame, **kwargs):
@@ -168,6 +156,212 @@ def orders(order_data: DataFrame, asset_price_data: DataFrame, **kwargs):
     figsize = get_param_default_if_missing("figsize", (10,6), **kwargs)
     _, axis = pyplot.subplots(figsize=figsize, sharex=True, sharey=False)
 
+    ticker = order_data.ticker.iloc[0]
+    run_id = order_data.run_id.iloc[0]
+    ensemble_id = order_data.ensemble_id.iloc[0]
+
+    title = f"{ticker} Order Size and Value\nRun ID: {run_id}, Ensemble ID: {ensemble_id}"
+
+    __orders(axis, order_data, asset_price_data, title=title)
+
+
+def order_pnl(data: DataFrame, **kwargs):
+    """
+    Plot profit and loss time series computed from orders.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Data to plot.
+    figsize : Tuple[int, int]
+        Figure size.
+    """
+
+    pnl(data.query('order_status == "Completed"'))
+
+
+def trade_pnl(data: DataFrame, **kwargs):
+    """
+    Plot profit and loss time series computed from trades.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Data to plot.
+    figsize : Tuple[int, int]
+        Figure size.
+    """
+
+    pnl(data.query('status == "Closed"'))
+
+
+def pnl(data: DataFrame, **kwargs):
+    """
+    Plot profit and loss time series. Inputs data must be a closed trade or order,
+
+    Parameters
+    ----------
+    data : DataFrame
+        Data to plot.
+    figsize : Tuple[int, int]
+        Figure size.
+    """
+
+    figsize = get_param_default_if_missing("figsize", (10,8), **kwargs)
+    _, axis = pyplot.subplots(2, figsize=figsize, sharex=True, sharey=False)
+
+    ticker = data.ticker.iloc[0]
+    run_id = data.run_id.iloc[0]
+    ensemble_id = data.ensemble_id.iloc[0]
+
+    title = f"{ticker} Profit and Loss\nRun ID: {run_id}, Ensemble ID: {ensemble_id}"
+
+    __pnl(axis, data, title=title)
+
+
+def zscore_backtest(broker: DataFrame, zscore_indicator: DataFrame, position: DataFrame, asset: DataFrame, 
+                    orders: DataFrame, mean_reversion_half_life: int, **kwargs):
+    """
+    Plot backtest results.
+
+    Parameters
+    ----------
+    broker : DataFrame
+        Broker data.
+    indicator : DataFrame
+        Indicator data.
+    position : DataFrame
+        Position data.
+    asset : DataFrame
+        Asset data.
+    orders : DataFrame
+        Order data.
+    mean_reversion_half_life : int
+        Mean reversion half life for price series.
+    figsize : Tuple[int, int]
+        Figure size.
+    """
+
+    figsize = get_param_default_if_missing("figsize", (10,10), **kwargs)
+
+    fig = pyplot.figure(constrained_layout=True, figsize=figsize)
+    spec = gridspec.GridSpec(ncols=1, nrows=4, figure=fig)
+
+    ax1 = fig.add_subplot(spec[0, 0])
+    ax2 = fig.add_subplot(spec[1:, 0])
+
+    __cash_value(ax1, broker)
+    __orders(ax2, orders, asset)
+
+
+"""
+Reusable plot components.
+
+__zscore_indicator
+    Plot zscore indicator time series.
+__asset_price
+    Plot asset price series.
+__pnl
+    Plot profit and loss time series.
+__orders
+    Plot when orders type occurs compared with asset price.
+__cash_value
+"""
+def __zscore_indicator(axis: pyplot.axis, data: DataFrame, mean_reversion_half_life: int, **kwargs):
+    """
+    Plot zscore indicator time series.
+
+    Parameters
+    ----------
+    axis : matplotlib.pyplot.axis
+        Axis used to draw plot.
+    data : DataFrame
+        Data to plot.
+    mean_reversion_half_life : int
+        Mean reversion half life for price series.
+    """
+
+    title = get_param_default_if_missing("title", None, **kwargs)
+
+
+    date = data.date.to_numpy()
+    zscore = data.zscore.to_numpy()
+    mean_reversion_half_life = int(mean_reversion_half_life)
+
+    zero = numpy.full(len(zscore), 0.0)
+
+    comp.comparison(axis, [zscore[mean_reversion_half_life:], zero], date[mean_reversion_half_life:], title=title, xlabel="Date", 
+                    ylabel="Z-Score", lw=1)
+
+
+def __asset_price(axis: pyplot.axis, data: DataFrame, **kwargs):
+    """
+    Plot asset price series.
+
+    Parameters
+    ----------
+    axis : matplotlib.pyplot.axis
+        Axis used to draw plot.
+    data : DataFrame
+        Data to plot.
+    title : str
+        Figure title.
+    """
+
+    title = get_param_default_if_missing("title", None, **kwargs)
+
+    date = data.date.to_numpy()
+    close = data.close_price.to_numpy()
+    ticker = data.ticker.iloc[0]
+    run_id = data.run_id.iloc[0]
+    ensemble_id = data.ensemble_id.iloc[0]
+
+    comp.curve(axis, close, date, title=title, xlabel="Date", ylabel="Price", label=ticker, lw=1)
+
+
+def __pnl(axis: pyplot.axis, data: DataFrame, **kwargs):
+    """
+    Plot profit and loss time series.
+
+    Parameters
+    ----------
+    axis : matplotlib.pyplot.axis
+        Axis used to draw plot.
+    data : DataFrame
+        Data to plot.
+    title : str
+        Figure title.
+    """
+
+    title = get_param_default_if_missing("title", None, **kwargs)
+
+    pnl = data.pnl.to_numpy()
+    pnl_date = data.date.to_numpy()
+
+    cumulative_pnl = data.pnl.cumsum().to_numpy()
+
+    comp.positive_negative_bar(axis[0], pnl, pnl_date, title=title, ylabel='PnL (Dollars)', alpha=1.0, xlabel=None)
+    comp.curve(axis[1], cumulative_pnl, pnl_date, ylabel='Value (Dollars)', xlabel='Date')
+
+
+def __orders(axis: pyplot.axis, order_data: DataFrame, asset_price_data: DataFrame, **kwargs):
+    """
+    Plot order size and value time series.
+
+    Parameters
+    ----------
+    axis : matplotlib.pyplot.axis
+        Axis used to draw plot.
+    data : DataFrame
+        Data to plot.
+    symbol_offset_factor : float
+        Symbol offset factor.
+    title : str
+        Figure title.
+    """
+
+    title = get_param_default_if_missing("title", None, **kwargs)
+
     price = asset_price_data.close_price.to_numpy()
     price_date = asset_price_data.date.to_numpy()
     offset = (numpy.max(price) - numpy.min(price)) * 0.05
@@ -181,40 +375,37 @@ def orders(order_data: DataFrame, asset_price_data: DataFrame, **kwargs):
     sell_date = sell_orders.date.to_numpy()
 
     ticker = sell_orders.ticker.iloc[0]
-    run_id = sell_orders.run_id.iloc[0]
-    ensemble_id = sell_orders.ensemble_id.iloc[0]
     
-    title = f"{ticker} Order Size and Value\nRun ID: {run_id}, Ensemble ID: {ensemble_id}"
-
     comp.fcurve_scatter_comparison(axis, [buy_price, sell_price], price, [buy_date, sell_date], price_date, title=title, xlabel='Date', 
-                                    ylabel='Price', lw=1, labels=[ticker, 'Buy', 'Sell'], markers=['^', 'v'], marker_colors=['#007735', '#BB0000'],
-                                    marker_size=6.0) 
+                                   ylabel='Price', lw=1, labels=[ticker, 'Buy', 'Sell'], markers=['^', 'v'], marker_colors=['#007735', '#BB0000'],
+                                   marker_size=6.0) 
 
 
-def pnl(data: DataFrame, **kwargs):
+
+def __cash_value(axis: pyplot.axis, data: DataFrame, **kwargs):
     """
-    Plot profit and loss time series.
+    Plot cash and value time series.
 
     Parameters
     ----------
+    axis : matplotlib.pyplot.axis
+        Axis used to draw plot.
     data : DataFrame
         Data to plot.
-    figsize : Tuple[int, int]
-        Figure size.
+    title : str
+        Figure title.
     """
 
-    figsize = get_param_default_if_missing("figsize", (10,8), **kwargs)
-    _, axis = pyplot.subplots(2, figsize=figsize, sharex=True, sharey=False)
+    title = get_param_default_if_missing("title", None, **kwargs)
 
-    pnl = data.pnl.to_numpy()
-    pnl_date = data.date.to_numpy()
-    ticker = data.ticker.iloc[0]
-    run_id = data.run_id.iloc[0]
-    ensemble_id = data.ensemble_id.iloc[0]
+    date = data.date.to_numpy()
+    cash = data.cash.to_numpy()
+    value = data.value.to_numpy()
+    spend = value - cash
+    run_id = data.run_id[0]
+    ensemble_id = data.ensemble_id[0]
 
-    cumulative_pnl = data.pnl.cumsum().to_numpy()
+    comp.comparison(axis, [cash, value, spend], date, title=title, xlabel="Date", ylabel="Dollars", lw=1, 
+                    labels=["Cash", "Value", "Spend"])
 
-    title = f"{ticker} Profit and Loss\nRun ID: {run_id}, Ensemble ID: {ensemble_id}"
 
-    comp.positive_negative_bar(axis[0], pnl, pnl_date, title=title, ylabel='PnL (Dollars)', alpha=1.0, xlabel=None)
-    comp.curve(axis[1], cumulative_pnl, pnl_date, ylabel='Value (Dollars)', xlabel='Date')
